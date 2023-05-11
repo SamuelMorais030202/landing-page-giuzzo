3a167c54ec9738922e4df68d4aac3f13fb583407
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-05-10 21:13:45 -0300

    Estilizando os cards de conhaça a giuzzo e a seção de noivas

---
+++ frontend/src/App.jsx
@@ -0,0 +1,66 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='body'>
+      <header>
+        <div className='giuzzo'>
+          <h1>Conheça a Giuzzo</h1>
+          <p>
+            Realce sua beleza única com a GIUZZO. Descubra nossa maquiagem de qualidade,
+            realce sua essência e brilhe. Junte-se a nós nessa jornada de autodescoberta
+            e empoderamento.
+          </p>
+          <button className='btn'>Venha nós conhecer</button>
+        </div>
+      </header>
+      <main>
+        <section className='noivas'>
+          <div className='div-noivas'>
+            <div className='contaudo-noivas'>
+              <h1>Noivas</h1>
+              <p>
+                Deixe-nos ser parte do dia mais importante da sua vida. Prepare-se para brilhar
+                intensamente no seu grande dia. Confie em nós para tornar cada momento inesquecível. 
+              </p>
+              <button className='btn'>Agende seu dia de noiva</button>
+            </div>
+          </div>
+        </section>
+        <section className='automaquiagem'>
+          <div>
+            <h1>Curso automaquiagem</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, rem eius.
+              Optio consequatur totam aliquid magni sed nesciunt aspernatur possimus assumenda
+              itaque maxime nihil ipsa quia, minus ad reprehenderit facilis?
+            </p>
+            <button>Venha aprender a se maquiar</button>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h1>Lip Glow Descubra seu novo vício</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, commodi porro cupiditate
+              at harum consectetur iste, eveniet a ipsum vitae repellat voluptatum iusto sed magni ex odio
+              tempore rem optio!
+            </p>
+            <button>Venha descobrir seu novo vício</button>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <section>
+          <div>
+            <h1>Tik tok GRWM</h1>
+            <p></p>
+            <button></button>
+          </div>
+        </section>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
