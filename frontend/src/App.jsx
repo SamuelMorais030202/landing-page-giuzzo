9ae0c04052bdde88587d0bf72ea4929170b7669c
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-05-16 21:18:50 -0300

    Desenvolvendo a landing page para celulares

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,51 +3,78 @@ import './App.css';
 function App() {
   return (
     <div className='body'>
-      <header>
+      <div className='sumario'>
         <div className='giuzzo'>
-          <h1>Conheça a Giuzzo</h1>
+          <h1>G.Pantuzzo</h1>
           <p>
-            Realce sua beleza única com a GIUZZO. Descubra nossa maquiagem de qualidade,
-            realce sua essência e brilhe. Junte-se a nós nessa jornada de autodescoberta
-            e empoderamento.
+            Giulia Pantuzzo é uma artista multidisciplinar que traduz seu estudo da Arte da Beleza em
+            todas as suas criações. Suas produções únicas provocam experiências que rompem com a
+            normalidade e celebram a potência do feminino.
           </p>
-          <button className='btn'>Venha nós conhecer</button>
+          <button className='btn'>
+            Contato e Agendamento
+          </button>
+          <button className='btn'>
+            <a href="#noivas">Dia de Noiva</a>
+          </button>
+          <button className='btn'>
+            <a href="#automaquiagem">Curso de Automaquiagem</a>
+          </button>
+          <button className='btn'>
+            <a href="#giuzzo">Conheça a Giuzzo</a>
+          </button>
+          <button className='btn'>
+            <a href="#lipGlow">Compre o Lip Glow</a>
+          </button>
         </div>
-      </header>
-      <main>
-        <section className='noivas'>
+      </div>
+      <section className='noivas' id='noivas'>
           <div className='div-noivas'>
             <div className='contaudo-noivas'>
               <h1>Noivas</h1>
               <p>
-                Deixe-nos ser parte do dia mais importante da sua vida. Prepare-se para brilhar
-                intensamente no seu grande dia. Confie em nós para tornar cada momento inesquecível. 
+                Para mim, é uma honra ser escolhida para produzir a Beleza do seu Dia de Noiva. Venha ceder o seu
+                rosto como tela para que juntas possamos concretizar o seu sonho para este dia especial. Desejo fazer
+                de você uma verdadeira obra prima, te deixando mais confiante do que nunca!
               </p>
               <button className='btn'>Agende seu dia de noiva</button>
             </div>
           </div>
-        </section>
-        <section className='breack'></section>
-        <section className='automaquiagem'>
+      </section>
+      <section className='automaquiagem' id='automaquiagem'>
           <div className='div-automaquiagem'>
             <div className='giuzzo'>
               <h1>Curso automaquiagem</h1>
               <p>
-                Descubra o poder da sua beleza.
-                Aprenda a destacar suas melhores qualidades e se sentir confiante
-                e radiante todos os dias. 
+                Quer aprender a ressaltar sua beleza de um jeito divertido e artístico? A “Arte da Beleza” é o meu
+                Curso de Automaquiagem em um formato de um Brunch & Make, onde irei desmistificar o mundo da
+                beleza para você e passar todos os meus segredos dessa Arte.
               </p>
               <button className='btn'>Venha aprender a se automaquiar</button>
             </div>
           </div>
         </section>
-        <section className='lipglow'>
+      <header>
+        <div className='giuzzo' id='giuzzo'>
+          <h1>GIUZZO</h1>
+          <p>
+            GIUZZO, uma marca de arte idealizada por Giulia Pantuzzo, para materializar o seu universo criativo.
+            Através de uma estética inovadora e posicionamento subversivo, suas criações não passam
+            despercebidas. A GIUZZO é um convite para experimentar a arte no seu dia a dia. 
+          </p>
+          <button className='btn'>Conheça a GIUZZO</button>
+        </div>
+      </header>
+      <main>
+        <section className='breack'></section>
+        <section className='lipglow' id='lipGlow'>
           <div>
             <div className='text-glow'>
               <h1>Lip Glow</h1>
               <p>
-                Não perca a oportunidade de se sentir confiante e deslumbrante. Deixe o Lip Glow ser
-                o seu segredo de beleza e encante a todos com seus lábios irresistíveis!
+                Hidratação, Refrescância, Arte e Beleza em um só potinho. O Lip Glow é o hidratante labial formulado
+                para realçar a sua beleza natural. Com uma combinação exclusiva de ativos, este produto foi
+                desenhado para incentivar o seu autocuidado e aumentar sua autoconfiança. 
               </p>
               <button className='btn'>Venha descobrir seu novo vício</button>
             </div>
@@ -55,12 +82,14 @@ function App() {
         </section>
       </main>
       <footer>
-        <section>
-          <div>
-            <h1>Tik tok GRWM</h1>
-            <p></p>
-            <button></button>
-          </div>
+        <section className='links'>
+          <h1>Me encontre</h1>
+          <nav>
+            <a href="https://www.instagram.com/gpantuzzo/" target="_blank" rel="noreferrer">Intagram de Art <span></span><span></span></a>
+            <a href="https://www.instagram.com/giuliapantuzzobeauty/" target="_blank" rel="noreferrer">Intagram Beauty <span></span> <span></span></a>
+            <a href="https://www.instagram.com/giuzzo.co/" target='_blank' rel="noreferrer">Intagram Giuzzo <span></span> <span></span></a>
+            <a href="https://www.tiktok.com/@gpantuzzo" target="_blank" rel="noreferrer">TikTok <span></span> <span></span></a>
+          </nav>
         </section>
       </footer>
     </div>
