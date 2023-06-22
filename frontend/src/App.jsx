6a46932bed12b9be9fcaf8a50df20ad2ff0dad94
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-06-22 00:24:38 -0300

    Começando a adicionar funcionalidades

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import './desktop.css';
 
 import { useEffect } from 'react';
+import queryString from 'query-string';
 
 // window.addEventListener('scroll', function() {
     //   const scrolledY = window.pageYOffset;
@@ -38,6 +39,20 @@ function App() {
     };
 
   }, []);
+
+  const submitWhatsApp = () => {
+    const number = 38988228793;
+    const message = 'Bem vindo';
+
+    const queryParams = queryString.stringify({
+      phone: number,
+      text: message,
+    });
+
+    const url = `whatsapp://send?${queryParams}`;
+  
+    window.location.href = url;
+  }
 
   return (
     <div className='body'>
@@ -81,7 +96,7 @@ function App() {
                 rosto como tela para que juntas possamos concretizar o seu sonho para este dia especial. Desejo fazer
                 de você uma verdadeira obra prima, te deixando mais confiante do que nunca!
               </p>
-              <button className='btn'>DIA DE NOIVA</button>
+              <button className='btn' onClick={ () => submitWhatsApp() }>DIA DE NOIVA</button>
             </div>
           </div>
       </section>
