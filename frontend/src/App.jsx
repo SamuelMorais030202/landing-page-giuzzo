19ac53a942ad265f721e13a76aed55bc01e6f3eb
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-06-21 23:09:24 -0300

    Efeito parallax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,14 @@ function App() {
   // window.addEventListener('scroll', handleScroll);
 
   useEffect(() => {
+      const handleScroll = () => {
+        const scrolledY = window.scrollY;
+        document.querySelector('.sumario').style.backgroundPosition = `left ${scrolledY}px`;
+        document.querySelector('.noivas').style.backgroundPosition = `left ${scrolledY - 480}px`;
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
     // window.addEventListener('scroll', function() {
     //   const scrolledY = window.pageYOffset;
 
@@ -30,26 +38,9 @@ function App() {
     //     element.style.backgroundPosition = 'left ' + newPosition + 'px';
     //   });
     // });
-    const handleParallax = () => {
-      const scrolledY = window.pageYOffset;
-  
-      const images = [
-        { id: '.sumario', initialPosition: 0 },
-        { id: '.noivas', initialPosition: 500 },
-        { id: '.automaquiagem', initialPosition: 900 }
-      ];
-  
-      images.forEach(function (image) {
-        const element = document.querySelector(image.id);
-        // const newPosition = image.initialPosition - scrolledY;
-        element.style.backgroundPosition = 'left ' + scrolledY + 'px';
-      });
-    };
-
-    window.addEventListener('scroll', handleParallax);
 
     return () => {
-      window.removeEventListener('scroll', handleParallax);
+      window.removeEventListener('scroll', handleScroll);
     };
 
   }, []);
