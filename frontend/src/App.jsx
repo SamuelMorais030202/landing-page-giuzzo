11ae66326a62e4f653b3382ef9babec91ea3f0f5
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-06-21 23:33:39 -0300

    Ajustando scrolledY

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
 
   useEffect(() => {
       const handleScroll = () => {
-        const scrolledY = window.scrollY * 0.4;
+        const scrolledY = window.scrollY * 0.3;
         document.querySelector('.sumario').style.backgroundPosition = `left ${scrolledY}px`;
-        document.querySelector('.noivas').style.backgroundPosition = `left ${scrolledY - 500}px`;
+        document.querySelector('.noivas').style.backgroundPosition = `left ${scrolledY - 400}px`;
         // document.querySelector('.automaquiagem').style.backgroundPosition = `left ${scrolledY - 1880}px`;
       };
 
